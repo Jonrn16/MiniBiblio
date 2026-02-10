c9625fedafe39706ed88a4f040855bbd2f1b92b7
jon risueño <jrisnav1606@g.ecuaand,es>
2026-02-10 14:05:31 +0100

    Creadas las rutas y sus respectivas plantillas en LibroController.php

--- src/Controller/LibroController.php
+++ src/Controller/LibroController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Autor;
+use App\Repository\AutorRepository;
 use App\Repository\LibroRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,9 +22,10 @@ class LibroController extends AbstractController
     }
 
     #[Route("/libro/autores/{id}", name: "libro_autores")]
-    public function autorPorId(Autor $autor)
+    public function autorPorId(LibroRepository $libroRepository, int $id) : Response
     {
-
+        $libro = $libroRepository->find($id);
+        return $this->render("libroAutores.html.twig",["libro" => $libro]);
     }
 
 }
