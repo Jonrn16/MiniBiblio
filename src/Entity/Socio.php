000ea964cc1b64f635d79027c58a0f65d5d2eebb
jon risueño <jrisnav1606@g.ecuaand,es>
2026-02-23 09:06:00 +0100

    Version con Socio incluido

---
+++ src/Entity/Socio.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SocioRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SocioRepository::class)]
+#[ORM\Table(name: 'socio')]
+class Socio
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', unique: true)]
+    private string $dni;
+
+    #[ORM\Column(type: 'string')]
+    private string $apellidos;
+
+    #[ORM\Column(type: 'string')]
+    private string $nombre;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $esDocente;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $esEstudiante;
+
+}
---
+++ src/Repository/SocioRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Socio;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Socio|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Socio|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Socio[] findAll()
+ * @method Socio[] findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SocioRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Socio::class);
+    }
+}
